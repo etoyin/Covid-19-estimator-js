5add73a12312e4e94167baedd356f9a0db0fa51b
Toyin Adesina <toyinadesina60@gmail.com>
2020-04-08 17:35:35 +0100

    changed from 4 spaces to 2 spaces added property shorthand

--- src/estimator.js
+++ src/estimator.js
@@ -1,7 +1,7 @@
 
-export const convertToDays = (periodType, timeToElapse) => {
+export const convertToDays = ( periodType, timeToElapse ) => {
   let timeInDays;
-  switch (periodType){
+  switch ( periodType ){
     case 'months':
       timeInDays = timeToElapse * 30;
       break;
@@ -14,47 +14,47 @@ export const convertToDays = (periodType, timeToElapse) => {
   return timeInDays;
 };
 
-export const impactCases = (data) => {
-  const {periodType, timeToElapse, reportedCases, population, totalHospitalBeds, region} = data;
+export const impactCases = ( data ) => {
+  const { periodType, timeToElapse, reportedCases, population, totalHospitalBeds, region } = data;
   const currentlyInfected = reportedCases * 10;
   const timeInDays = convertToDays(periodType, timeToElapse);
-  const infectionsByRequestedTime = currentlyInfected * ( 2 ** Math.floor(timeInDays/3) );
-  const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
-  const hospitalBedsByRequestedTime = Math.floor(totalHospitalBeds * 0.35) - severeCasesByRequestedTime;
-  const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime * 0.05);
-  const casesForVentilatorsByRequestedTime = Math.floor(infectionsByRequestedTime * 0.02);
+  const infectionsByRequestedTime = currentlyInfected * ( 2 ** Math.floor( timeInDays / 3 ) );
+  const severeCasesByRequestedTime = Math.floor( infectionsByRequestedTime * 0.15 );
+  const hospitalBedsByRequestedTime = Math.floor( totalHospitalBeds * 0.35 ) - severeCasesByRequestedTime;
+  const casesForICUByRequestedTime = Math.floor( infectionsByRequestedTime * 0.05 );
+  const casesForVentilatorsByRequestedTime = Math.floor( infectionsByRequestedTime * 0.02 );
   const dollarsInFlight = region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * timeInDays;
   
   return {
-    currentlyInfected: currentlyInfected,
-    infectionsByRequestedTime: infectionsByRequestedTime,
-    severeCasesByRequestedTime: severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
-    casesForICUByRequestedTime: casesForICUByRequestedTime,
-    casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime,
-    dollarsInFlight: dollarsInFlight
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   }
 }
 
-export const severeImpactCases = (data) => {
-  const {periodType, timeToElapse, reportedCases, population, totalHospitalBeds, region} = data;
+export const severeImpactCases = ( data ) => {
+  const { periodType, timeToElapse, reportedCases, population, totalHospitalBeds, region } = data;
   const currentlyInfected = reportedCases * 50;
-  const timeInDays = convertToDays(periodType, timeToElapse);
-  const infectionsByRequestedTime = currentlyInfected * ( 2 ** Math.floor(timeInDays/3) );
-  const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
-  const hospitalBedsByRequestedTime = Math.floor(totalHospitalBeds * 0.35) - severeCasesByRequestedTime;
-  const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime * 0.05);
-  const casesForVentilatorsByRequestedTime = Math.floor(infectionsByRequestedTime * 0.02);
+  const timeInDays = convertToDays( periodType, timeToElapse );
+  const infectionsByRequestedTime = currentlyInfected * ( 2 ** Math.floor( timeInDays / 3 ) );
+  const severeCasesByRequestedTime = Math.floor( infectionsByRequestedTime * 0.15 );
+  const hospitalBedsByRequestedTime = Math.floor( totalHospitalBeds * 0.35 ) - severeCasesByRequestedTime;
+  const casesForICUByRequestedTime = Math.floor( infectionsByRequestedTime * 0.05 );
+  const casesForVentilatorsByRequestedTime = Math.floor( infectionsByRequestedTime * 0.02 );
   const dollarsInFlight = region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * timeInDays;
   
   return {
-    currentlyInfected: currentlyInfected,
-    infectionsByRequestedTime: infectionsByRequestedTime,
-    severeCasesByRequestedTime: severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime: hospitalBedsByRequestedTime,
-    casesForICUByRequestedTime: casesForICUByRequestedTime,
-    casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime,
-    dollarsInFlight: dollarsInFlight
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   }
 }
 
